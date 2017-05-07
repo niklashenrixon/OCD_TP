c8ae99cb02d51a77e516d9b68db4bd066474739a
Niklas Henrixon <info@innovationbynixon.se>
2017-05-07 22:18:03 +0200

    Added file structure
    Added file structure

---
+++ @OCD_TP/mod.cpp
@@ -0,0 +1,12 @@
+version		= "1.0";
+name        = "OCD Transport Protocol";
+author      = "Nixon";
+picture     = "logo.paa";
+logo        = "logo.paa";
+logoOver    = "logo.paa";
+actionName  = "Website";
+action      = "http://github.com/niklashenrixon/OCD_TP";
+tooltip     = "OCD Transport Protocol";
+overview    = "OCD Transport Protocol";
+description = "OCD Transport Protocol";
+tooltip		= "OCD Transport Protocol";
