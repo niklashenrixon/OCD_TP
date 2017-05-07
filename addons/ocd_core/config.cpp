1e32ec5147af2c0804002d6edd13ecf9a914303f
Niklas Henrixon <info@innovationbynixon.se>
2017-05-07 23:46:45 +0200

    cats
    lots of cats

--- addons/ocd_core/config.cpp
+++ addons/ocd_core/config.cpp
@@ -17,28 +17,25 @@
 */ //////////////////////////////////////////////////////////////////
 
 class CfgMods {
-
-    class OCD_TP {
-        dir         = "@OCD_TP";
-        picture     = "\ocd_core\logo.paa";
-        name        = "OCD Transport Protocol";
-        author      = "Nixon";
-        hidePicture = 0;
-        hideName    = 0;
-        version     = "1.0";
-    };
-    author = "Nixon";
+	class OCD_CORE {
+		dir         = "@OCD_TP";
+		picture     = "\ocd_core\logo.paa";
+		name        = "OCD Transport Protocol";
+		author      = "Nixon";
+		hidePicture = 0;
+		hideName    = 0;
+		version     = "1.0";
+	};
+	author = "Nixon";
 };
 
-class CfgPatches
-{
-    class OCD_TP
-    {
-        name             = "OCD Transport Protocol";
-        author           = "Nixon";
-        authorUrl        = "http://github.com/niklashenrixon/OCD_TP";
-        version          = "1.0";
-        requiredVersion  = 1.0;
-        requiredAddons[] = {};
-    };
+class CfgPatches {
+	class OCD_CORE {
+		name             = "OCD Transport Protocol";
+		author           = "Nixon";
+		authorUrl        = "http://github.com/niklashenrixon/OCD_TP";
+		version          = "1.0";
+		requiredVersion  = 1.0;
+		requiredAddons[] = {};
+	};
 };
